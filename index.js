44045a307684ce378e6fd8c123967bd18a858510
skj_og <95894160+Jha-Shubham19@users.noreply.github.com>
2024-04-07 13:19:37 +0530

    using express

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-import http from 'node:http';
-import {URL} from 'node:url';
-import {handleRandom, handleDefineQueryWord} from './request_handlers.js';
+import express from 'express';
+import { URL } from 'url';
+import { handleRandom, handleDefineQueryWord } from './request_handlers.js';
 
-
-const host = 'localhost';
+const app = express();
 const port = process.env.PORT || 8000;
 
-const requestListener = async function (req, res) {
-    const parsedUrl = new URL(req.url, 'http://localhost');
-    const queryWord = parsedUrl.searchParams.get('q');
-    const fullUrl = parsedUrl.href;
+app.use(express.urlencoded({ extended: true }));
 
-    console.log(req.url.substring(1), parsedUrl, queryWord, parsedUrl.href);
+app.get('/', (req, res) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  handleRandom(res, fullUrl);
+});
 
-    if(parsedUrl.pathname === '/random' || parsedUrl.pathname === '/') {
-        handleRandom(res,fullUrl);
-    }
-    else if (parsedUrl.pathname === '/define' && queryWord) {
-        handleDefineQueryWord(res, fullUrl, queryWord);
-    }
-    else {
-        res.writeHead(404);
-        res.end("Nothing there");
-    }
+app.get('/random', (req, res) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  handleRandom(res, fullUrl);
+});
 
-    
-};
+app.get('/define', (req, res) => {
+  const queryWord = req.query.q;
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-const server = http.createServer(requestListener);
-server.listen(port, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  if (queryWord) {
+    handleDefineQueryWord(res, fullUrl, queryWord);
+  } else {
+    res.status(400).send('Query word is missing');
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('Nothing there');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- request_handlers.js
+++ request_handlers.js
@@ -1,36 +1,34 @@
 import { providePhoto } from './making_photo.js';
 import fetch from 'node-fetch';
 
-async function handleDefineQueryWord(res, fullUrl , queryWord) {
-    const photo = await providePhoto(queryWord);
+async function handleDefineQueryWord(res, fullUrl, queryWord) {
+  const photo = await providePhoto(queryWord);
 
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(`<html>
-                <head>
-                <meta property="og:description" content="Meaning of ${queryWord}"/>
-                <meta property="og:site_name" content="_site_name"/>
-                <meta property="og:title" content="${queryWord}"/>
-                <meta property="og:url" content="${fullUrl}"/>
-                <meta property="og:image" content="${photo}"/>
-                <meta property="og:type" content="website"/>
-                </head>
-                <body>
-                    <h1>This is ${queryWord}</h1>
-                    <img src = "${photo}" alt="not_found"/>
-                </body>
-            </html>`);
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(`
+    <html>
+      <head>
+        <meta property="og:description" content="Meaning of ${queryWord}" />
+        <meta property="og:site_name" content="_site_name" />
+        <meta property="og:title" content="${queryWord}" />
+        <meta property="og:url" content="${fullUrl}" />
+        <meta property="og:image" content="${photo}" />
+        <meta property="og:type" content="website" />
+      </head>
+      <body>
+        <h1>This is ${queryWord}</h1>
+        <img src="${photo}" alt="not_found" />
+      </body>
+    </html>
+  `);
 }
 
-
 async function handleRandom(res, fullUrl) {
-    let randomWord = await fetch(`https://random-word-api.herokuapp.com/word`);
-    randomWord = await randomWord.json();
+  let randomWord = await fetch('https://random-word-api.herokuapp.com/word');
+  randomWord = await randomWord.json();
 
-    const queryWord = randomWord[0];
-    handleDefineQueryWord(res, fullUrl, queryWord);
-    
-    // console.log(handleRandom);
+  const queryWord = randomWord[0];
+  handleDefineQueryWord(res, fullUrl, queryWord);
 }
 
 export { handleRandom, handleDefineQueryWord };
