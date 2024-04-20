879ffe810ca9bd4071e1b476a62a5b23685ca713
skj_og <95894160+Jha-Shubham19@users.noreply.github.com>
2024-04-20 14:32:26 +0530

    .

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,16 +1,9 @@
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
-import { Configuration } from 'puppeteer';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const {join} = require('path');
 
 /**
- * @type {Configuration}
+ * @type {import("puppeteer").Configuration}
  */
-const configuration = {
+module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
-
-export default configuration;
